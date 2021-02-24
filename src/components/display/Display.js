2be19d8be47cbf42cdcbbd9d0e90c19bd2fccf2c
g4rrold <voronkov.g4@gmail.com>
2021-02-24 22:42:28 +0200

    Make display auto-scalable

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './Display.scss';
 
 export const Display = (props) => {
+  const ref = useRef();
+  const [scale, setScale] = useState(1);
   const { getResult, firstOperand, secondOperand, operation, isError } = props;
+
+  useEffect(() => {
+    if (ref.current) {
+      const node = ref.current;
+      const parentNode = ref.current.parentNode;
+      const availableWidth = parentNode.offsetWidth;
+      const actualWidth = node.offsetWidth;
+      const actualScale = availableWidth / actualWidth;
+
+      if (scale === actualScale) return;
+
+      if (actualScale < 1) {
+        setScale(actualScale);
+      } else if (scale < 1) {
+        setScale(1);
+      }
+    }
+  }, [firstOperand, secondOperand, operation]);
 
   return (
     <div className="display calculator-display">
-      <p className="display-main-line">{`${firstOperand ?? ''} ${operation ?? ''} ${
-        secondOperand ?? ''
-      }`}</p>
+      <p ref={ref} style={{ transform: `scale(${scale}, ${scale})` }} className="display-main-line">
+        {`${firstOperand ?? ''} ${operation ?? ''} ${secondOperand ?? ''}`}
+      </p>
       <p className="display-result-line">{isError ? 'Error' : getResult() ?? firstOperand}</p>
     </div>
   );
